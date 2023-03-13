7e4a0f16eed9adc42f7d63f46606a25c2e7374c4
QianLu <qian.lu@nets-tech.com>
2023-03-13 17:37:46 +0800

    Add version command and init token command

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
@@ -10,7 +9,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
+// var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -45,7 +44,5 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
---
+++ cmd/token.go
@@ -0,0 +1,34 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// tokenCmd represents the token command
+var tokenCmd = &cobra.Command{
+	Use:   "token",
+	Short: "Set or Get your OpenAI Token",
+	Long:  `Set or Get your OpenAI Token,`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(args)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(tokenCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// tokenCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// tokenCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
