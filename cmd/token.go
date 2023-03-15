b00139151d2bc0ec93adea0d31f5c3965395dea4
QianLu <qian.lu@nets-tech.com>
2023-03-15 13:33:01 +0800

    finish token get set method

--- cmd/token.go
+++ cmd/token.go
@@ -4,13 +4,16 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/CirillaQL/kubectl-openai/pkg/log"
 	"github.com/CirillaQL/kubectl-openai/pkg/token"
 	"github.com/spf13/cobra"
 )
 
 var openAIToken string
-var tokenPath string
+
+const tokenPath = "$HOME/.kube/kubectl-openai"
 
 // tokenCmd represents the token command
 var tokenCmd = &cobra.Command{
@@ -18,10 +21,15 @@ var tokenCmd = &cobra.Command{
 	Short: "Set or Get your OpenAI Token",
 	Long:  `Set or Get your OpenAI Token,`,
 	Run: func(cmd *cobra.Command, args []string) {
-		log.Info(openAIToken)
+		openAIToken = args[0]
 		err := token.SaveToken(openAIToken, tokenPath)
 		if err != nil {
 			log.Fatalf("save token into file failed. error: %v", err)
+		}
+		token, err := token.ReadToken(tokenPath)
+		fmt.Println(token)
+		if err != nil {
+			log.Fatalf("load token failed. error: %v", err)
 		}
 	},
 }
@@ -34,7 +42,6 @@ func init() {
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
 	// tokenCmd.PersistentFlags().String("foo", "", "A help for foo")
-	tokenCmd.Flags().StringVarP(&tokenPath, "path", "p", "~/.kube/kubectl-openai", "custom file path")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
